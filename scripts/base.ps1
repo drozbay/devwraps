a39ad7fe8ce0ad55dbcb88210fd9c87746b81871
ozbayb <17261091+ozbayb@users.noreply.github.com>
2024-03-14 12:14:37 -0600

    Update Get-Anaconda-Path function to include fallback locations

--- scripts/base.ps1
+++ scripts/base.ps1
@@ -31,7 +31,8 @@ Function Get-Anaconda-Path {
     process {
         $bases = "Registry::HKEY_CURRENT_USER\Software\Python\PythonCore"
         if (-not (Test-Path $bases)) {
-            Write-Error "Cannot find Anaconda. Is Anaconda for Python 3 installed (https://www.anaconda.com/download)?" -ErrorAction Stop
+            Write-Host "Cannot find Anaconda in the default location. Falling back to alternative locations."
+            return Get-Anaconda-Path-Fallback
         }
 
         $subs = (Get-Item -LiteralPath $bases).GetSubKeyNames()
@@ -50,8 +51,46 @@ Function Get-Anaconda-Path {
         else {
             $item = Get-Item -LiteralPath $addr
             $anacondaPath = $item.GetValue("")
-			Write-Host "Anaconda Path: $anacondaPath"  # Display the path on the screen
+            Write-Host "Anaconda Path: $anacondaPath"  # Display the path on the screen
             return $anacondaPath
+        }
+    }
+}
+
+Function Get-Anaconda-Path-Fallback {
+    process {
+        $bases = "Registry::HKEY_LOCAL_MACHINE\Software\Python\ContinuumAnalytics", "Registry::HKEY_CURRENT_USER\Software\Python\ContinuumAnalytics"
+        $found = -1
+        for ($i = 0; $i -lt $bases.Count; $i++) {
+            if (Test-Path $bases[$i]) {
+                $found = $i
+                break;
+            }
+        }
+
+        if ($found -lt 0) {
+            Write-Error "Cannot find Anaconda. Is Anaconda for Python 3 installed (https://www.anaconda.com/download)?" -ErrorAction Stop
+        }
+
+        $base = $bases[$i]
+        $subs = (Get-Item -LiteralPath $base).GetSubKeyNames()
+        $addr = ""
+        for ($i = 0; $i -lt $subs.Count; $i++) {
+            $item = Get-Item -LiteralPath ($base + "\" + $subs[$i])
+            $version = $item.GetValue("SysVersion") -as [decimal]
+            if ($version -gt 3) {
+                $addr = $base + "\" + $subs[$i]
+            }
+        }
+
+        if ($addr.Length -lt 1) {
+            Write-Error "Anaconda for Python 3+ must be installed (Anaconda for Python 2 is not supported)" -ErrorAction Stop
+        }
+        else {
+            $item = Get-Item -LiteralPath "$addr\InstallPath"
+            $item = ($item).GetValue("ExecutablePath")
+            $item = (Split-Path -Parent -Path $item)
+            return $item
         }
     }
 }
@@ -65,12 +104,11 @@ Function Activate-Anaconda {
     process {
         Write-Host "Activating Anaconda environment: $environmentName"
         $anacondaPath = Get-Anaconda-Path
-        $anacondaPath = (Split-Path -Parent -Path $anacondaPath)
-        $envPath = Join-Path -Path $anacondaPath -ChildPath "envs\$environmentName"
-
+        $condaEnvPath = Join-Path -Path $anacondaPath -ChildPath "envs\$environmentName"
+        
         # https://github.com/BCSharp/PSCondaEnvs
-        $env:Path = "$envPath;$envPath\Library\mingw-w64\bin;$envPath\Library\usr\bin;$envPath\Library\bin;$envPath\Scripts;$envPath\bin;" + $env:Path
+        $env:Path = "$condaEnvPath;$condaEnvPath\Library\mingw-w64\bin;$condaEnvPath\Library\usr\bin;$condaEnvPath\Library\bin;$condaEnvPath\Scripts;$condaEnvPath\bin;" + $env:Path
         $env:CONDA_DEFAULT_ENV = $environmentName
-        $env:CONDA_PREFIX = $envPath
+        $env:CONDA_PREFIX = $condaEnvPath
     }
 }
